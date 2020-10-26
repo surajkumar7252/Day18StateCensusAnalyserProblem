8ba875ea1286c7ceda2e9fea85c9d6b0355012ef
Surajmal Kumar <surajkumar7252@gmail.com>
2020-10-26 23:12:42 +0530

    [Surajmal Kumar].Adding UC3. adding ability to report sorted census data in json format

--- src/main/java/day18statecensusanalyserproblem/StateCensusAnalyser.java
+++ src/main/java/day18statecensusanalyserproblem/StateCensusAnalyser.java
@@ -3,8 +3,10 @@ package day18statecensusanalyserproblem;
 import java.io.*;
 import java.nio.file.*;
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.StreamSupport;
 
+import com.google.gson.Gson;
 import com.opencsv.bean.*;
 import com.opencsv.exceptions.*;
 import org.apache.commons.lang3.exception.ExceptionUtils;
@@ -65,7 +67,38 @@ public class StateCensusAnalyser
 				throw new CsvException("File Error", typeOfCsvException.FILE_ERROR);
 			}
 		   
+}
+	public void sortingData(List<CSVStateCensus> csvCensusList, Comparator<CSVStateCensus> censusComparator) {
+		int i=0;
+		while(i<csvCensusList.size()-1) {
+			int j=0;
+		  while( j<csvCensusList.size()-i-1) {
+				CSVStateCensus tempData1=csvCensusList.get(j);
+				CSVStateCensus tempData2=csvCensusList.get(j+1);
+				if(censusComparator.compare(tempData1, tempData2)>0) {
+					csvCensusList.set(j+1, tempData1);
+					csvCensusList.set(j, tempData2);
+					
+				}j++;
+			}i++;
+		}
 	}
+	
+	 public String sortedCensusPerState(String csvFilePath, TypeOfCsvBuilder typeOfCsvBuilder) throws CensusException, CsvException {
+			try (Reader fileReader = Files.newBufferedReader(Paths.get(csvFilePath));) {
+				    ICsvCreator csvCreator = (typeOfCsvBuilder==TypeOfCsvBuilder.OPEN_CSV?CsvBuilderFactory.createBuilderEntry():CsvBuilderFactory.createCommonBuilder());
+					List<CSVStateCensus>csvStateCensusList = csvCreator.getCSVList(fileReader, CSVStateCensus.class);
+				Function<CSVStateCensus, String> key=census->census.stateName;
+				Comparator<CSVStateCensus> comparator=Comparator.comparing(key);
+				sortingData(csvStateCensusList, comparator);
+				String sortedJsonCensus=new Gson().toJson(csvStateCensusList);
+				return sortedJsonCensus;
+			} catch (IOException e) {
+				throw new CsvException("File Error", typeOfCsvException.FILE_ERROR);
+			}
+		   
+		}
+	
 }
 
 	
